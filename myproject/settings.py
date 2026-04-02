a55a6f541cb7a14d743309028f061278e3fec615
Ubuntu <ubuntu@ip-172-31-65-145.ec2.internal>
2026-04-02 11:25:36 +0000

    Initial commit

--- myproject/settings.py
+++ myproject/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-temp-key'
 DEBUG = True
 
-ALLOWED_HOSTS = ['54.173.28.199', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['98.92.52.0', 'localhost', '127.0.0.1']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
